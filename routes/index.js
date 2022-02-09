672d56565a1760ba1fb2e34164f7064ad081f574
Pool <pooldeveloper08@gmail.com>
2022-02-09 16:55:09 -0500

    quitando /api en los endpoints

--- routes/index.js
+++ routes/index.js
@@ -4,15 +4,17 @@ const schoolController = require('../controllers/schoolController');
 
 module.exports = function () {
     //Crea una nueva escuela
-    router.post('/api/new-school',
+    router.post('/new-school',
         schoolController.newSchool,
     );
+
     //Obtiene la escuela por su subdominio
-    router.get('/api/:subdomain',
+    router.get('/:subdomain',
         schoolController.getSchool,
     )
+    
     //Realizar el pago a stripe
-    router.post('/api/:subdomain/checkout',
+    router.post('/:subdomain/checkout',
         schoolController.checkout,
     );
     return router;
